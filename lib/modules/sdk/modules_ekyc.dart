6a8c118e371a5c9eadf6f1a29af630819f99aff4
lochv <lochv@intern.softdreams.vn>
2025-02-11 20:10:23 +0700

    update isFromModules

--- lib/modules/sdk/modules_ekyc.dart
+++ lib/modules/sdk/modules_ekyc.dart
@@ -1,6 +1,6 @@
+import 'package:module_ekyc/assets.dart';
 import 'package:module_ekyc/base_app/base_app.src.dart';
 
-import '../../core/router/app_router.src.dart';
 import '../../shares/shares.src.dart';
 import '../authentication_kyc/nfc_kyc/nfc_kyc.src.dart';
 import 'sdk.src.dart';
@@ -10,6 +10,7 @@ class ModulesEkyc {
     AppController appController = Get.put(AppController());
 
     appController.isOnlyNFC = true;
+    Assets.isFromModules = true;
     var result = await appController.checkPermissionApp();
     Get.delete<AppController>();
     return result;
@@ -18,6 +19,7 @@ class ModulesEkyc {
   static Future<SendNfcRequestModel?> checkEKYC(
       SdkRequestModel sdkRequestModel) async {
     AppController appController = Get.put(AppController());
+    Assets.isFromModules = true;
     appController.sdkModel = sdkRequestModel;
     var result = await appController.checkPermissionApp();
     Get.delete<AppController>();
